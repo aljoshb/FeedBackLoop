fb5bad4bb95ad58bd4323c3eca6ca9f0102ba585
joshuaalawode <joshalawode@gmail.com>
2018-07-14 20:40:24 -0400

    Added logout feature

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
-const passport = require('passport'); // The original passport module in node_modules
+const passport = require('passport');
 
-module.exports = (app) => { // Export the routes so they can be used in index.js to add these methods to the app object
+module.exports = (app) => {
 
     /* Make a user google login request to Google */
     app.get(
@@ -12,6 +12,12 @@ module.exports = (app) => { // Export the routes so they can be used in index.js
 
     /* A user's google login request response will get sent back to this route from Google */
     app.get('/auth/google/callback', passport.authenticate('google'));
+
+    /* Log the user out */
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
 
     /* Just testing the authentication flow works */
     app.get('/api/current_user', (req, res) => {
