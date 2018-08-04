ebc07a66d6c4fed7989177b7a4e26afaa62dd169
joshuaalawode <joshalawode@gmail.com>
2018-08-04 19:25:36 -0400

    Set up billing in the app

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/user');
 require('./services/passport'); // passport.js does not actually export anything, we just want it to be here when the server starts up so that it will run
 
 /* Connect to the MongoDB database using mongoose */
 mongoose.connect(keys.mongoURI);
 
-/* Create an instance of the express */
+/* Create an instance of express */
 const app = express();
 
-/* Enable cookies inside the application, set the Cookie-Session header */
-app.use(
+/* Set up the middlewares */
+app.use(bodyParser.json()); // Middleware to parse the request and make it accessible as a json object like: req.body.<whatever>
+app.use( // Middleware to set the cookie
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds. This is how long the cookie can be used in the browser before it expires
         keys: [keys.cookieKey] // Used to sign/encrypt the cookie, so that someone else does not give us this cookie
     })
 );
-
-/* Tell passport to use cookies to handle authentication */
-app.use(passport.initialize());
+app.use(passport.initialize()); // Middleware to add the current user to the request object
 app.use(passport.session());
 
+/* Handle all the routes */
 require('./routes/authRoutes')(app); // Instead of doing: const authRoutes = require('./routes/authRoutes'); then: authRoutes(app)
+require('./routes/billingRoutes')(app);
 
 /* Set PORT for production vs development environment */
-const PORT = process.env.PORT || 5000; // Heroku sets the PORT as an env variable
+const PORT = process.env.PORT || 5000; // Heroku sets the PORT as an env variable. If PORT env doesn't exist, we are in development so use 5000
 app.listen(PORT);
--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose; // instead of: const Schema = mongoose.Schema;
 
 /* Create a user Schema: describes what a user will look like */
 const userSchema = new Schema({
-    googleId: String
+    googleId: String,
+    credits: { type: Number, default: 0 }
 });
 
 /* Create an actual model class and tell mongoose that the user collection needs to be created */
